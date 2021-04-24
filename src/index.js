fc7e663640cb344fe4d9d7ad67a733d1bc6677ac
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-04-25 00:51:38 +0700

    Static file && SCSS

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ const handlebars  = require('express-handlebars');
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Templates engine
 app.engine('hbs', handlebars({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.render('home')
