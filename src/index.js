4768ea1a1bf8c9b4d623747eb723db8de143ae99
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-04-25 17:21:48 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ const handlebars  = require('express-handlebars');
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({
+  extended:true
+})); //dạng form đã có urlencode xử lý
+app.use(express.json()); //xử lý javascript
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Templates engine
 app.engine('hbs', handlebars({
@@ -20,11 +25,21 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home')
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news')
-})
+});
+
+app.get('/search', (req, res) => {
+  // console.log(req.query.ref);
+  res.render('search')
+});
+
+app.post('/search', (req, res) => {
+  // console.log(req.body);
+  res.send('')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
