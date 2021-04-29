2be37a04c93ee47415ffe386be0dd58c04f04dfd
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-04-29 18:24:30 +0700

    READ from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,22 +1,17 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
   //[GET] /
-  index(req, res) {
-
-    Course.find({}, function (err, courses) {
-      if(!err)
-      {
-        res.json(courses);
-      } else{
-        res.status(400).json({ error: 'ERROR!!!' });
-      }
-      
-    });
-
-    // res.render('home');
+  index(req, res, next) {
+    Course.find({})
+      .then(courses => {
+        res.render('home', { 
+          courses: mutipleMongooseToObject(courses)
+        });
+      })
+      .catch(next);
   }
-
   //[GET]/search
   search(req, res) {
     res.render('search');
